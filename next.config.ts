664d57364337ce9398ec6750d7d555e0a83cfa1e
zohreb <zohre.baniasadi86@gmail.com>
2026-06-06 13:01:08 +0200

    Rempve Github pages based path for custom domain

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/navaco-website",
-  assetPrefix: "/navaco-website/",
   images: {
     unoptimized: true,
   },
